00d7302888624828f555a06cf056cd8e9b25c19f
Ivan Antsiferov <i.antsiferov@hh.ru>
2021-03-27 12:57:41 +0300

    Add country page.'

--- lib/app/navigation/routes.dart
+++ lib/app/navigation/routes.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:s7hack/ui/counter/counter_page.dart';
+import 'package:s7hack/ui/country/country_page.dart';
 
 class Routes {
   const Routes._();
 
   static const home = '/';
+  static const country = "/country";
   static final table = <String, Route Function(RouteSettings)>{
     Routes.home: (settings) =>
         _defaultRoute(settings, (context) => CounterPage()),
+    Routes.country: (settings) =>
+        _defaultRoute(settings, (context) => CountryPage())
   };
 
   static Route<T> _defaultRoute<T>(
---
+++ lib/ui/country/country_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CountryPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    throw _CountryPageState();
+  }
+}
+
+class _CountryPageState extends State<CountryPage> {
+  @override
+  Widget build(BuildContext context) {
+    throw Text("COUNTRY SCREEN");
+  }
+}
