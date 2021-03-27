91b2a4c8795451b9f2de4fc98ea8f270f34ef337
Sergey Koltsov <srg.koltsov@yandex.ru>
2021-03-27 23:14:31 +0300

    Swapping items.

--- lib/ui/counter/counter_page.dart
+++ lib/ui/counter/counter_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:s7hack/ui/components/fling_detector.dart';
 
 class CounterPage extends StatefulWidget {
   @override
@@ -10,12 +11,12 @@ class _CounterPageState extends State<CounterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SafeArea(child: Board(array: array, rows: 4, columns: 4)),
+      body: SafeArea(
+          child: Board(
+              array: List.generate(16, (index) => index), rows: 4, columns: 4)),
     );
   }
 }
-
-final array = List.generate(16, (index) => index);
 
 class Board extends StatefulWidget {
   final List<int> array;
@@ -33,7 +34,22 @@ class Board extends StatefulWidget {
   _BoardState createState() => _BoardState();
 }
 
-class _BoardState extends State<Board> {
+class _BoardState extends State<Board> with TickerProviderStateMixin {
+  final _animController = <int, AnimationController>{};
+  final _animation = <int, Animation<Offset>>{};
+  final _tween = <int, Tween<Offset>>{};
+
+  final _array = <int>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _array.addAll(widget.array);
+    for (var i = 0; i < widget.array.length; i++) {
+      initAnim(i);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -48,8 +64,17 @@ class _BoardState extends State<Board> {
             SliverGrid.count(
               crossAxisCount: widget.columns,
               children: [
-                for (var i = 0; i < array.length; i++)
-                  ValueView(value: array[i], size: itemSize),
+                for (var i = 0; i < _array.length; i++)
+                  FlingDetector(
+                    onRightFling: () => moveRight(i),
+                    onLeftFling: () => moveLeft(i),
+                    onUpFling: () => moveUp(i),
+                    onDownFling: () => moveDown(i),
+                    child: SlideTransition(
+                      position: _animation[_array[i]]!,
+                      child: ValueView(value: _array[i], size: itemSize),
+                    ),
+                  ),
               ],
             )
           ],
@@ -57,6 +82,75 @@ class _BoardState extends State<Board> {
       },
     );
   }
+
+  void initAnim(int index) {
+    final value = _array[index];
+    final animController =
+        AnimationController(duration: Duration(milliseconds: 300), vsync: this);
+    final tween = Tween(begin: Offset.zero, end: Offset(0, 1));
+    final animation = tween.animate(animController);
+    _animController[value] = animController;
+    _animation[value] = animation;
+    _tween[value] = tween;
+  }
+
+  Future<void> move(int index, AxisDirection direction) async {
+    int toIndex;
+    final from = _array[index];
+    int to;
+    switch (direction) {
+      case AxisDirection.up:
+        toIndex = index - widget.columns;
+        to = _array[toIndex];
+        _tween[from]?.end = Offset(0, -1);
+        _tween[to]?.end = Offset(0, 1);
+        break;
+      case AxisDirection.right:
+        toIndex = index + 1;
+        to = _array[toIndex];
+        _tween[from]?.end = Offset(1, 0);
+        _tween[to]?.end = Offset(-1, 0);
+        break;
+      case AxisDirection.down:
+        toIndex = index + widget.columns;
+        to = _array[toIndex];
+        _tween[from]?.end = Offset(0, 1);
+        _tween[to]?.end = Offset(0, -1);
+        break;
+      case AxisDirection.left:
+        toIndex = index - 1;
+        to = _array[toIndex];
+        _tween[from]?.end = Offset(-1, 0);
+        _tween[to]?.end = Offset(1, 0);
+        break;
+    }
+    await Future.wait([
+      _animController[from]!.forward(),
+      _animController[to]!.forward(),
+    ]);
+
+    initAnim(index);
+    initAnim(toIndex);
+
+    _swap(index, toIndex);
+  }
+
+  void _swap(int a, int b) {
+    final tmp = _array[a];
+    _array[a] = _array[b];
+    _array[b] = tmp;
+    setState(() {});
+    print('Swapped: $a, $b');
+    print(_array);
+  }
+
+  Future<void> moveLeft(int index) => move(index, AxisDirection.left);
+
+  Future<void> moveRight(int index) => move(index, AxisDirection.right);
+
+  Future<void> moveUp(int index) => move(index, AxisDirection.up);
+
+  Future<void> moveDown(int index) => move(index, AxisDirection.down);
 }
 
 class ValueView extends StatelessWidget {
