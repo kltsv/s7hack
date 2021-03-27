e0c12503128f56dac2a425352a2abd31dfb822ee
Ivan Antsiferov <i.antsiferov@hh.ru>
2021-03-27 21:55:29 +0300

    Implement remove collapsed items from field.

--- lib/domain/engine/differ.dart
+++ lib/domain/engine/differ.dart
@@ -6,8 +6,8 @@ import 'models/item.dart';
 import 'models/item_diff.dart';
 import 'models/item_type.dart';
 
-Set<ItemDiff> calcCollapsingDiff(List<List<Item>> field) {
-  final Set<ItemDiff> diffs = {};
+Set<ItemDiffExplosion> calcCollapsingDiff(List<List<Item>> field) {
+  final Set<ItemDiffExplosion> diffs = {};
   final int rows = field.length;
   final int columns = rows > 0 ? (field.getSafe(0)?.length ?? 0) : 0;
 
@@ -18,9 +18,41 @@ Set<ItemDiff> calcCollapsingDiff(List<List<Item>> field) {
       final ItemType type = item.type;
       final Index index = Index(i, j);
       final Set<Index> matches = findMatches(field, type, index);
-      diffs.addAll(matches.map((e) => ItemDiff.explosion(e)));
+      diffs.addAll(matches.map((e) => ItemDiffExplosion(e)));
     }
   }
 
   return diffs;
+}
+
+List<List<Item?>> removeCollapsed(
+  List<List<Item>> field,
+  Set<ItemDiffExplosion> collapsingDiff,
+) {
+  final List<List<Item?>> newField =
+      field.map((r) => r.map((c) => c as Item?).toList()).toList();
+  collapsingDiff.forEach((element) {
+    newField[element.index.i][element.index.j] = null;
+  });
+  return newField;
+}
+
+List<ItemDiffChange> calcChangeDiff(
+  List<List<Item?>> collapsedField,
+  Set<ItemDiffExplosion> collapsingDiff,
+) {
+  final diff = <ItemDiffChange>[];
+  final Set<int> affectedColumns = collapsingDiff.map((e) => e.index.j).toSet();
+  final int rows = collapsedField.length;
+  final int columns = rows > 0 ? (collapsedField.getSafe(0)?.length ?? 0) : 0;
+
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < columns; j++) {
+
+    }
+  }
+
+  collapsingDiff.forEach((element) {});
+
+  return [];
 }
--- test/differ_test.dart
+++ test/differ_test.dart
@@ -116,4 +116,28 @@ void main() {
 
     expect(calcCollapsingDiff(field), expected);
   });
+
+  test("test removing collapsed items", () {
+    final List<List<Item>> field = [
+      [
+        Item(0, ItemType.bag),
+        Item(1, ItemType.auto),
+        Item(2, ItemType.diamond)
+      ],
+      [Item(3, ItemType.plane), Item(4, ItemType.auto), Item(5, ItemType.auto)],
+      [Item(6, ItemType.shield), Item(7, ItemType.auto), Item(8, ItemType.bag)],
+    ];
+    final Set<ItemDiffExplosion> collapsingDiff = {
+      ItemDiffExplosion(Index(0, 1)),
+      ItemDiffExplosion(Index(1, 1)),
+      ItemDiffExplosion(Index(2, 1)),
+    };
+    final List<List<Item?>> expected = [
+      [Item(0, ItemType.bag), null, Item(2, ItemType.diamond)],
+      [Item(3, ItemType.plane), null, Item(5, ItemType.auto)],
+      [Item(6, ItemType.shield), null, Item(8, ItemType.bag)],
+    ];
+
+    expect(removeCollapsed(field, collapsingDiff), expected);
+  });
 }
