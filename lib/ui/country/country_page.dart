8ab8e7ccf3690c69fd430d9770a2b144ee1d6a89
Sergey Koltsov <srg.koltsov@yandex.ru>
2021-03-27 14:43:28 +0300

    View fix.

--- lib/ui/country/country_page.dart
+++ lib/ui/country/country_page.dart
@@ -5,19 +5,19 @@ import 'country_level_item.dart';
 
 class CountryPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    throw _CountryPageState();
-  }
+  State<StatefulWidget> createState() => _CountryPageState();
 }
 
 class _CountryPageState extends State<CountryPage> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return Scaffold(
+      body: ListView.builder(
         itemCount: icelandData.length,
         itemBuilder: (BuildContext context, int index) {
           return CountryLevelItem(icelandData[index]);
-        }
+        },
+      ),
     );
   }
 }
